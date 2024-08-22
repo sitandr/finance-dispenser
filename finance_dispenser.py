49c87f809ef3044087e591bf77a42df4e429a461
sitandr <andr-sitnikov@mail.ru>
2024-08-22 12:21:04 +0300

    Preamble & rounding

---
+++ finance_dispenser.py
@@ -0,0 +1,140 @@
+import argparse
+from dataclasses import dataclass
+import csv
+from fractions import Fraction
+from itertools import groupby
+from termcolor import colored
+import yaml
+# pylint: disable-next=wildcard-import
+from utils import *
+
+parser = argparse.ArgumentParser(
+    description="A program that helps dividing the expenses for a group of people.")
+parser.add_argument("prefix", default="real_data/", nargs="?",
+            help="""Prefix to be added to the names of files.
+            For example: \"data/my_\" -> data/my_items.csv, data/my_rules.yml""")
+parser.add_argument("--round", type=int, default=2, help="Number of digits to round the numbers")
+
+args = parser.parse_args()
+file = args.prefix + "items.csv"
+
+rules = yaml.safe_load(open(args.prefix + "rules.yml", encoding="utf-8"))
+
+groups = rules['groups']
+
+groups = {n: set(g) for (n, g) in groups.items()}
+all_p = set.union(*groups.values())
+
+def check_exists(v: str|set[str]|list[str]) -> None:
+    if isinstance(v, set):
+        for s in v:
+            assert s in all_p, "\"" + str(v) + "\" not in names list: " + str(all_p)
+    elif isinstance(v, str):
+        assert v in all_p, "\"" + str(v) + "\" not in names list: " + str(all_p)
+    else:
+        raise ValueError(str(v) + " is not a name or set of names")
+
+@dataclass
+class Transaction:
+    '''Data class that describes single transaction'''
+    from_: str|set[str]
+    to_: str|set[str]
+    name: str
+    value: float
+    frac: int|float|Fraction
+    comment: str
+    order: int
+
+operations: list[Transaction] = []
+balance: dict[str, list[float]] = {p: [] for p in all_p }
+
+def pay(_from, _to, frac=1, comment="", order=0):
+    check_exists(_from)
+    check_exists(_to)
+
+    assert isinstance(this['name'], str), f"Transaction name: \"{this["name"]}\" must be string"
+    assert isinstance(this["price"], int|float|Fraction), f"Transaction sum: \"{this["price"]}\" must be number"
+    assert isinstance(frac, int|float|Fraction), f"Transaction fraction \"{frac}\" must be number"
+    assert isinstance(comment, str), f"Transaction name: \"{comment}\" must be string"
+    assert isinstance(order, int|float|Fraction), f"Transaction fraction \"{order}\" must be number"
+    operations.append(Transaction(
+        _from, _to, this["name"], float(this["price"]), frac, comment, order))
+
+def just_pay(_from, _to, name, value, frac=1, comment="", order=0):
+    check_exists(_from)
+    check_exists(_to)
+
+    assert isinstance(name, str), f"Transaction name: \"{name}\" must be string"
+    assert isinstance(value, int|float|Fraction), f"Transaction sum: \"{value}\" must be number"
+    assert isinstance(frac, int|float|Fraction), f"Transaction fraction \"{frac}\" must be number"
+    assert isinstance(comment, str), f"Transaction name: \"{comment}\" must be string"
+    assert isinstance(order, int|float|Fraction), f"Transaction fraction \"{order}\" must be number"
+
+    operations.append(Transaction(_from, _to, name, float(value), frac, comment, order))
+
+if "preamble" in rules:
+    # pylint: disable-next=exec-used
+    exec(rules['preamble'], {"all": all_p, "Frac": Fraction, "just_pay": just_pay} | groups)
+
+with open(file, newline="", encoding="utf-8") as f:
+    reader = csv.DictReader(f, delimiter=";")
+    for item in reader:
+        item = {n: convert_value(v) for (n, v) in item.items()}
+
+        this = item
+        var = {} # local variables
+
+        # pylint: disable-next=exec-used
+        exec(rules['rules'],
+             item | {"all": all_p, "pay": pay, "Frac": Fraction, "just_pay": just_pay, "var": var}
+             | groups)
+
+operations.sort(key=lambda op:
+    (op.order,
+     op.comment,
+     hash(next(iter(into_iterable(op.from_)))),
+     hash(next(iter(into_iterable(op.to_))))))
+
+print()
+print()
+
+for (k, transact) in groupby(operations, key=lambda op: (op.from_, op.to_, op.frac, op.comment)):
+    transact = list(transact)
+
+    names = [colored(t.name, "cyan") + colored(f"({display_float(t.value)})", "yellow")
+             for t in transact]
+    money = sum((t.value for t in transact)) * k[2]
+
+    from_cost = float(money / check_len(k[0]))
+    to_cost = float(money / check_len(k[1]))
+
+    for p in into_iterable(k[0]):
+        balance[p].append(-from_cost)
+
+    for p in into_iterable(k[1]):
+        balance[p].append(to_cost)
+
+    from_cost = display_float(round(from_cost, args.round))
+    to_cost = display_float(round(to_cost, args.round))
+
+    print(colored(group_display(k[0]), "magenta"), colored("\t->", "magenta"),
+          check_len(k[0]), "×", colored(f"{from_cost:7}", "red"), end=" ")
+    inner = f"{f"(x{str(round_any(k[2], 2 + max(0, args.round)))})" if k[2] != 1 else "":-^15}"
+    print(colored("\t--", "magenta")
+          + colored(inner, "light_cyan") + colored("->\t", "magenta"), end=" ")
+    print(check_len(k[1]), "×", colored(to_cost, "yellow"),
+          colored("\t->", "magenta"), colored(group_display(k[1]), "green"), end=": \t")
+    print(f"{(", ".join(names)):36}", end="")
+    if k[3] != "":
+        print(colored("\t# " + k[3], "dark_grey"), end="")
+    print()
+
+print()
+
+for p in sorted(balance):
+    print(f"{p:10}", end=": \t")
+    print(" ".join(
+        (f"{balance_color(f"{display_float(display_float(round(b, args.round))):+}", b).replace("+", "+ ").replace("-", "- "):9}"
+         for b in balance[p])), end="\n\t\t")
+    print("=", balance_color(round(sum(balance[p]), args.round)))
+    print()
--- utils.py
+++ utils.py
@@ -1,36 +1,42 @@
 from fractions import Fraction
 from termcolor import colored
 
-def convert_value(s: str|None) -> str|int|float|Fraction|None:
+
+def convert_value(s: str | None) -> str | int | float | Fraction | None:
     if s == None or len(s) == 0:
         return None
-    
+
     if s.isdigit():
         return int(s)
-    
-    if s.replace('.','',1).isdigit():
+
+    if s.replace(".", "", 1).isdigit():
         return float(s)
-    
-    
-    if s.replace('/','',1).isdigit():
+
+    if s.replace("/", "", 1).isdigit():
         return Fraction(s)
-    
+
     return s
 
-def check_len(s: str|list[str]|set[str]):
+
+def check_len(s: str | list[str] | set[str]):
     if type(s) == str:
+        return 1
+    if len(s) == 0:
         return 1
     return len(s)
 
-def group_display(s: str|list[str]|set[str]) -> str:
+
+def group_display(s: str | list[str] | set[str]) -> str:
     if type(s) == set or type(s) == list:
         return str(", ".join(sorted(s)))
     return str(s)
 
-def into_iterable(s: str|list[str]|set[str]):
+
+def into_iterable(s: str | list[str] | set[str]):
     if type(s) == str:
         return [s]
     return s
+
 
 def balance_color(s, n=None) -> str:
     if n == None:
@@ -41,10 +47,17 @@ def balance_color(s, n=None) -> str:
         return colored(s, "red")
     return colored(s, "white")
 
-def round_any(n: int|float|Fraction, digits=2):
+
+def round_any(n: int | float | Fraction, digits=2):
     if type(n) == float:
         return round(n, digits)
     if type(n) == Fraction:
         if n.denominator > 10:
             return round(float(n), digits)
+    return n
+
+
+def display_float(n: float) -> int | float:
+    if n % 1 == 0:
+        return int(n)
     return n
